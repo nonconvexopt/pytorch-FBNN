9c1878d9757fe8ab2d58db7717577ee3e99d2fd0
Kim Juhyeong <nonconvexopt@gmail.com>
2021-12-27 19:46:24 +0900

    Add 'setup.py'.

--- inference/__init__.py
+++
@@ -1,4 +0,0 @@
-from .fvi import fvi
-from .adversarial_fvi import adversarial_fvi
-from .random_fvi import random_fvi
-from .testpoint_fvi import testpoint_fvi
---
+++ setup.py
@@ -0,0 +1,26 @@
+from glob import glob
+from os.path import basename, splitext
+from setuptools import find_packages, setup
+
+
+install_requires = [
+    'torch>=1.9.0',
+    'gpytorch>=1.6.0',
+    ]
+
+dependency_links = [
+    'git+https://github.com/nonconvexopt/pytorch-SSGE',
+    ]
+
+setup(
+    name='pytorch_fbnn',
+    version='0.1',
+    author='Juhyeong Kim',
+    author_email='nonconvexopt@gmail.com',
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    license='MIT',
+    py_modules=['estimator', 'model'],
+    packages=['pytorch_fbnn'],    
+)
