206a0282fcda594b16929c8e01ec2e9dbb3d7b1b
Kim Juhyeong <nonconvexopt@gmail.com>
2021-12-29 00:01:49 +0900

    Initial structure of Functional Variational Inference.

--- estimator/fvi.py
+++ estimator/fvi.py
@@ -1,26 +1,32 @@
+from typing import Union
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import gpytorch
 
-_fvi_mode = [
-    'random',
+from .inducing import inducing_sampler
+
+
+__fvi_mode__ = [
+    'sampling_based',
     'adversarial',
     'test_point',   
 ]
+
 
 class fvi(nn.Module):
     def __init__(
         self,
         variational_model: nn.Module,
         prior: gpytorch.models.GP,
-        mode = 'random',
+        inducing_strategy: Union[str, inducing_sampler] = 'random',
         fn_noise_std = 1e-8,
     ):
         super(fvi, self).__init__()
 
-        assert mode in _fvi_mode
-        assert hasattr(variational_model, 'weight_mean') and hasattr(variational_model, 'weight_logvar')
+        assert hasattr(variational_model, 'weight_mean')
+        assert hasattr(variational_model, 'weight_logvar')
 
         # Variational model is assumed to have pytorch 'forward' method as a __call__ method and 'kld' method
         # returning KL divergence. Also, forward method is assumed to return stochastic results.
@@ -28,7 +34,19 @@ class fvi(nn.Module):
         self.prior = prior
         self.fn_noise_std = fn_noise_std
 
-    def self.fkld(self, x, fn_noise_std = 0.0):
+        if isinstance(mode, str):
+            assert inducing_strategy in __fvi_mode__, "Wrong inducing sample strategy specified."
+        else:
+            self.inducing_strategy = inducing_strategy
+
+        # Check if Variational Module performs stochastic forwarding.
+        with torch.no_grad():
+            assert self.variational_model(self.inducing_sample()) != self.variational_model(self.inducing_sample())
+        
+    def inducing_sample(self):
+        self.inducing_strategy.sample()
+
+    def fkld(self, x, fn_noise_std = 0.0):
         model_pred_dist = self.model(x)
         gp_pred_dist = self.prior(x)
         
@@ -44,10 +62,8 @@ class fvi(nn.Module):
             x = x.unsqueeze(0).repeat(1, repeat)
             inducing_x = inducing_x.unsqueeze(0).repeat(1, repeat)
             self.variational_model(x)
-            
-            
-            
+
         preds = [self.model(x) for ind in range(repeat)]
         fkld = self.fkld()
-        
+
         return preds, fkld
