6e198a6bd32b754813fb4519e18f91d0278304d1
Kim Juhyeong <nonconvexopt@gmail.com>
2021-12-29 23:13:41 +0900

    Initial implemenation.

--- estimator/inducing.py
+++ estimator/inducing.py
@@ -3,32 +3,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributions as dist
 
+
 class inducing_sampler:
-    def sampling(self) -> torch.Tensor:
-        raise NotImplementedError
+    # TODO: inherit PyTorch Dataloader
+    input_shape = torch.Size()
 
 
 class sampling_based_inducing(inducing_sampler):
-    def __init__(self, scalar_dist: dist.distribution.Distribution, input_shape: torch.Size, mean=None, std=None):
-        self.scalar_dist = scalar_dist
+    def __init__(self, sample_dist: dist.distribution.Distribution, input_shape: torch.Size, mean=None, std=None):
+        self.sample_dist = sample_dist
         self.input_shape = torch.Size(input_shape)
 
-    def sampling(self, sample_size=torch.Size()) -> torch.Tensor:
+    def sampling(self, sample_size=torch.Size(), batch: torch.Tensor) -> torch.Tensor:
         shape = torch.Size(sample_size) + self.input_shape
-        return self.scalar_dist(shape) * std.expand(input_shape) + mean.expand(input_shape)
+        return self.sample_dist(shape) * std.expand(shape) + mean.expand(shape)
 
 
 class adversarial_inducing(inducing_sampler):
-    def __init__(self, dataloader: torch.utils.data.DataLoader):
-        self.dataloader = dataloader
+    def __init__(self, sample_dist: dist.distribution.Distribution, latent_shape: torch.Size, generator: nn.Module):
+        self.latent_shape = torch.Size(latent_shape)
+        self.generator = generator
 
-    def sampling(self) -> torch.Tensor:
-        return 
+    def sampling(self, sample_size=torch.Size(), batch: torch.Tensor) -> torch.Tensor:
+        shape = torch.Size(sample_size) + self.latent_shape
+        return self.generator(shape)
 
 
 class test_point_inducing(inducing_sampler):
     def __init__(self, test_loader: torch.utils.data.DataLoader):
         self.test_loader = test_loader
 
-    def sampling(self) -> torch.Tensor:
+    def sampling(self, sample_size=torch.Size(), batch: torch.Tensor) -> torch.Tensor:
         return self.test_loader.next()
